f8652fb61bfcac661c184375ed737db38a87620a
mihasK <mihas-91@tut.by>
2012-12-25 20:16:55 +0300

    Hello world

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: 04.12.2012
+ *      Author: 1278
+ */
+
+#include <openssl/crypto.h>
+#include <openssl/objects.h>
+#include <openssl/engine.h>
+
+static int bind_fn(ENGINE * e, const char *id)
+{
+  if (!ENGINE_set_id(e, "simple") ||
+      !ENGINE_set_name(e, "simple engine")) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+IMPLEMENT_DYNAMIC_CHECK_FN();
+IMPLEMENT_DYNAMIC_BIND_FN(bind_fn);
