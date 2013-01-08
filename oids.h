399ad00c63d0f41de4fb390c5c5a953d063c598e
denis <denis@denis-HP-ProBook-4525s.(none)>
2013-01-09 00:47:46 +0300

    Oids file was added

---
+++ oids.h
@@ -0,0 +1,18 @@
+/*
+ * oids.h
+ *
+ *  Created on: 09.01.2013
+ *      Author: denis
+ */
+
+#ifndef BELT_OIDS_H_
+#define BELT_OIDS_H_
+
+// OIDs for BELT objects should start with "1.2.653.2.2"
+
+// Defines description of HBELT mesage digest algorithm
+#define OID_md_belt "1.2.653.2.2.9"
+#define SN_md_belt "md_belt"
+#define LN_md_belt "HBELT digest algorithm"
+
+#endif /* BELT_OIDS_H_ */
