47ea3630cf0d35bf538870298999d423edfa1970
denis <denis@denis-HP-ProBook-4525s.(none)>
2013-02-01 02:11:46 +0300

    Stub mac pmeth was added

--- belt_pmeth.c
+++ belt_pmeth.c
@@ -5,4 +5,47 @@
  *      Author: denis
  */
 
+#include "belt.h"
 
+EVP_PKEY_METHOD belt_pmeth_mac = {
+
+	NID_undef,
+	0, /* flags */
+
+	NULL, /* init */
+	NULL, /* copy */
+	NULL, /* cleanup */
+
+	NULL, /* paramgen_init */
+	NULL, /* paramgen */
+
+	NULL, /* keygen_init */
+	NULL, /* keygen */
+
+	NULL, /* sign_init */
+	NULL, /* sign */
+
+	NULL, /* verify_init */
+	NULL, /* verify */
+
+	NULL, /* verify_recover_init */
+	NULL, /* verify_recover */
+
+	NULL, /* signctx_init */
+	NULL, /* signctx */
+
+	NULL, /* verifyctx_init */
+	NULL, /* verifyctx */
+
+	NULL, /* encrypt_init */
+	NULL, /* encrypt */
+
+	NULL, /* decrypt_init */
+	NULL, /* decrypt */
+
+	NULL, /* derive_init */
+	NULL, /* derive */
+
+	NULL, /* ctrl */
+	NULL /* ctrl_str */
+};
