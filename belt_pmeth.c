ce3e2e556de626323a8ae53e6793514f889b847d
denis <denis@denis-HP-ProBook-4525s.(none)>
2013-02-01 03:14:11 +0300

    Pkey mac init, cleanup and copy were added

--- belt_pmeth.c
+++ belt_pmeth.c
@@ -13,6 +13,10 @@ struct belt_mac_pmeth_data {
 	unsigned char key[BELT_CIPHER_KEY_SIZE];
 };
 
+static int pkey_belt_mac_init(EVP_PKEY_CTX *ctx);
+static void pkey_belt_mac_cleanup(EVP_PKEY_CTX *ctx);
+static int pkey_belt_mac_copy(EVP_PKEY_CTX *dst, EVP_PKEY_CTX *src);
+
 static int pkey_belt_mac_keygen(EVP_PKEY_CTX *ctx, EVP_PKEY *pkey);
 
 static int pkey_belt_mac_signctx_init(EVP_PKEY_CTX *ctx, EVP_MD_CTX *mctx);
@@ -28,9 +32,9 @@ EVP_PKEY_METHOD belt_pmeth_mac = {
 	NID_undef,
 	0, /* flags */
 
-	NULL, /* init */
-	NULL, /* copy */
-	NULL, /* cleanup */
+	pkey_belt_mac_init, /* init */
+	pkey_belt_mac_copy, /* copy */
+	pkey_belt_mac_cleanup, /* cleanup */
 
 	NULL, /* paramgen_init */
 	NULL, /* paramgen */
@@ -65,6 +69,34 @@ EVP_PKEY_METHOD belt_pmeth_mac = {
 	pkey_belt_mac_ctrl, /* ctrl */
 	pkey_belt_mac_ctrl_str /* ctrl_str */
 };
+
+static int pkey_belt_mac_init(EVP_PKEY_CTX *ctx) {
+	struct belt_mac_pmeth_data *data;
+	data = OPENSSL_malloc(sizeof(struct belt_mac_pmeth_data));
+	if (!data) {
+		return 0;
+	}
+	memSetZero(data, sizeof(struct belt_mac_pmeth_data));
+	EVP_PKEY_CTX_set_data(ctx, data);
+	return 1;
+}
+
+static void pkey_belt_mac_cleanup(EVP_PKEY_CTX *ctx) {
+	struct belt_mac_pmeth_data *data = EVP_PKEY_CTX_get_data(ctx);
+	memSetZero(data, sizeof(struct belt_mac_pmeth_data));
+	OPENSSL_free(data);
+}
+
+static int pkey_belt_mac_copy(EVP_PKEY_CTX *dst, EVP_PKEY_CTX *src) {
+	struct belt_mac_pmeth_data *dst_data, *src_data;
+	if (!pkey_belt_mac_init(dst)) {
+		return 0;
+	}
+	src_data = EVP_PKEY_CTX_get_data(src);
+	dst_data = EVP_PKEY_CTX_get_data(dst);
+	*dst_data = *src_data;
+	return 1;
+}
 
 static int pkey_belt_mac_keygen(EVP_PKEY_CTX *ctx, EVP_PKEY *pkey) {
 	struct belt_mac_pmeth_data *data = EVP_PKEY_CTX_get_data(ctx);
